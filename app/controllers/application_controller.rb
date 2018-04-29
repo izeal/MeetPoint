c981a72b03e7781261a428cfbb8d832ff8c522f7
izeal <ivanz_89@mail.ru>
2018-04-29 15:57:02 +0300

    install devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :config_params_for_create, if: :devise_controller?
+  before_action :config_params_for_edit, if: :devise_controller?
+
+  def config_params_for_edit
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: [:password, :password_confirmation, :current_password]
+    )
+  end
+
+  def config_params_for_create
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :password])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   before_validation { email.downcase! }
