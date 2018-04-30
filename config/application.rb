70cadb33150d5cea8464b977d802f89a92fb6f70
izeal <ivanz_89@mail.ru>
2018-04-30 19:44:48 +0300

    add i18n

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,8 @@ module Meetpoint
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.time_zone = 'Moscow'
+    config.i18n.default_locale = :ru
   end
 end
