7a70ce26149217435f5368ea6f9bf83cb2b40fb0
izeal <ivanz_89@mail.ru>
2018-05-07 00:47:00 +0300

    fix word

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -40,7 +40,7 @@ class CommentsController < ApplicationController
   def notify_subscribers(event, comment)
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
     all_emails.each {
-      |mail| EvantMailer.comment(event, comment, mail).deliver_now
+      |mail| EventMailer.comment(event, comment, mail).deliver_now
     }
   end
 end
