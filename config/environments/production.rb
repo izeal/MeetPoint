20c09300f4b294c55eed47c2331a9486657b555a
izeal <ivanz_89@mail.ru>
2018-05-11 08:07:43 +0300

    try to fix delivery bug agan

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,10 +93,10 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    :address        => 'smtp.gmail.com',
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
