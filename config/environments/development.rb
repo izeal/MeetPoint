73bd8d50f4a401a5b77262b0ca29592e332d1d70
izeal <ivanz_89@mail.ru>
2018-05-11 07:52:19 +0300

    fix mail delivery bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: '587',
     user_name: 'testteeee53',
-    password: 'foobarbaz',
+    password: 'XXXXX',
     authentication: 'plain',
     enable_starttls_auto: true
   }
