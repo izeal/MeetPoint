1e93cb5e547c0dadf173f74261741afff80d4355
izeal <ivanz_89@mail.ru>
2018-05-11 07:57:04 +0300

    try to fix delivery bug agan

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     :address        => 'smtp.gmail.com',
     :port           => '587',
     :authentication => :plain,
