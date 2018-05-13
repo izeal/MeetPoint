b0bc5db24fa12ced87ac701b1c9fd569d4efdb8a
izeal <ivanz_89@mail.ru>
2018-05-13 18:49:59 +0300

    small refactoring

--- app/services/event_notifier.rb
+++ app/services/event_notifier.rb
@@ -1,32 +1,43 @@
 module EventNotifier
-  def self.execute(model)
+  extend self
+
+  def execute(model)
     case
-    when model.class == Comment
-      if model.destroyed?
-        EventMailer.comment_destroyed(model, model.user.email).deliver_now
-      else
-        emails = participants_emails(model)
-        emails.each { |mail| EventMailer.comment(model, mail).deliver_now }
-      end
-
-    when model.class == Photo
-      if model.destroyed?
-        EventMailer.photo_destroyed(model, model.user.email).deliver_now
-      else
-        emails = participants_emails(model)
-        emails.each { |mail| EventMailer.photo(model, mail).deliver_now }
-      end
-
-    when model.class == Subscription
-      if model.destroyed?
-        EventMailer.subscription_destroyed(model).deliver_now
-      else
-        EventMailer.subscription(model).deliver_now
-      end
+    when model.class == Comment then comment_sender(model)
+    when model.class == Photo then photo_sender(model)
+    when model.class == Subscription then subscription_sender(model)
     end
   end
 
-  def self.participants_emails(model)
+  private
+
+  def participants_emails(model)
     (model.event.subscriptions.preload(:user).map(&:user_email) + [model.event.user.email] - [model.user.try(:email)]).uniq
+  end
+
+  def comment_sender(model)
+    if model.destroyed?
+      EventMailer.comment_destroyed(model, model.user.email).deliver_now
+    else
+      emails = participants_emails(model)
+      emails.each { |mail| EventMailer.comment(model, mail).deliver_now }
+    end
+  end
+
+  def photo_sender(model)
+    if model.destroyed?
+      EventMailer.photo_destroyed(model, model.user.email).deliver_now
+    else
+      emails = participants_emails(model)
+      emails.each { |mail| EventMailer.photo(model, mail).deliver_now }
+    end
+  end
+
+  def subscription_sender(model)
+    if model.destroyed?
+      EventMailer.subscription_destroyed(model).deliver_now
+    else
+      EventMailer.subscription(model).deliver_now
+    end
   end
 end
